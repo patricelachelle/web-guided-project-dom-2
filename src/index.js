5df47ab3188466accf76851a9140213140efd02d
Andreas Bolos <andreasbolos@Andreass-MacBook-Pro.local>
2021-02-09 11:01:01 -0700

    wip

--- src/index.js
+++ src/index.js
@@ -34,6 +34,8 @@ launchButton.addEventListener('click', function (event) {
       `);
   })
 
+  
+
 // 👉 TASK 3- Create a function that launches!
 // It should open the confirmation modal.
 // Add it as an event listener for click events on the launch button.
